40925ae99f8d3c8b0469a02e4407c5a69ad16cbc
Nguyễn Như Hải Đăng <113578906+ezhentai@users.noreply.github.com>
2023-06-07 21:30:07 +0700

    Init page

---
+++ index.js
@@ -0,0 +1,32 @@
+function sign_up (e){
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var user = {
+        username : username,
+        email : email,
+        password : password,
+    }
+    var json = JSON.stringify(user);
+    localStorage.setItem(username, json)
+    alert("đăng kí thành công , vui lòng đăng nhập")
+    window.location.href="index.html"
+}
+function sign_in (e){
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var user = localStorage.getItem(username);
+    var data = JSON.parse(user);
+    if(user == null){
+        alert("Đăng nhập thất bại!")
+    }
+    else if(username == data.username && password == data.password){
+        alert("Đăng nhập thành công!")    
+        window.location.href="home.html"
+    }
+    else{
+        alert("Đăng nhập thất bại!")
+    }
+}
